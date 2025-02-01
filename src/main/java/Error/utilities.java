153d54d927530c68b43a478bb8347296949ebf85
PriyankaKumbhar152 <priyankakumbhar1381@gmail.com>
2025-02-01 23:55:04 +0530

    adding a method of excelsheet

--- src/main/java/Error/utilities.java
+++ src/main/java/Error/utilities.java
@@ -1,5 +1,30 @@
 package Error;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class utilities {
 
+	
+	public void readExcel() {
+    FileInputStream fis=null;
+    try {
+    	String filePath = null;
+		fis = new FileInputStream(filePath);
+    	}catch(FileNotFoundException e) {
+    		e.printStackTrace();
+    	}
+      Workbook book=null;
+      try {
+    	  book = new XSSFWorkbook(fis);
+      }catch (IOException e) {
+    	  e.printStackTrace();
+      }
+      Sheet sheet = book.getSheet("");
+	}
 }
